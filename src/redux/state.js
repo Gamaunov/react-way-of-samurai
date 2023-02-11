3a438722dbc778cc2c89e41722d64fe81f5438a6
Gamaunov <gamaunov1911@gmail.com>
2023-02-11 11:47:14 +0300

    creating reducer

--- src/redux/state.js
+++ src/redux/state.js
@@ -30,6 +30,7 @@ let store = {
       ],
       newMessageBody: ""
     },
+    sidebar: {}
   },
   getState() {
     return this._state;
@@ -42,27 +43,7 @@ let store = {
   },
 
   dispatch(action) {
-    if (action.type === ADD_POST) {
-      let newPost = {
-        id: 5,
-        message: this._state.profilePage.newPostText,
-        likesCount: 0,
-      };
-      this._state.profilePage.posts.push(newPost);
-      this._state.profilePage.newPostText = "";
-      this._callSubscriber(this._state);
-    } else if (action.type === UPDATE_NEW_POST_TEXT) {
-      this._state.profilePage.newPostText = action.newText;
-      this._callSubscriber(this._state);
-    } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-      this._state.dialogsPage.newMessageBody = action.body;
-      this._callSubscriber(this._state);
-    } else if (action.type === SEND_MESSAGE) {
-      let body = this._state.dialogsPage.newMessageBody;
-      this._state.dialogsPage.newMessageBody = "";
-      this._state.dialogsPage.messages.push({id: 6, message: body})
-      this._callSubscriber(this._state);
-    }
+
   },
 };
 
